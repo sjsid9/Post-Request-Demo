3c34ffd2a22e6bf52a6df5468740fd8956f26c7f
Siddhant <sjsid9@gmail.com>
2020-01-03 16:23:06 +0530

    redirecting page to a certain route and requet body
    Signed-off-by: Siddhant <sjsid9@gmail.com>

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
-const router = express.Router();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
+
+friends = ["tony", "Steve", "bucky", "bruce", "clark", "barry"];
 
 app.get("/", function (req, res) {
     res.render("home");
 });
 
 app.get("/friends", function (req, res) {
-    friends = ["tony", "Steve", "bucky", "bruce", "clark", "barry"];
     res.render("friends", { friends: friends });
 });
 
 app.post("/addfriend", function (req, res) {
-    res.send("You have reached the POST Route!!!")
+    // console.log(req.body.newfriend);
+    var friend = req.body.newfriend;
+    friends.push(friend);
+    res.redirect("/friends");
+    // res.send("You have reached the POST Route!!!")
 });
 
 app.listen(3000, function (req, res) {
     console.log("Server Started!!");
 })
-
-module.exports = router;
